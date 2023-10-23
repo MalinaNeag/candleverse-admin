c5cd6f3fa6e2fd315c5fa79bb6da745fe15e0f54
MalinaNeag <malina.neag@student.upt.ro>
2023-10-23 18:53:14 +0300

    Initial commit

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,36 @@
+import NextAuth, {getServerSession} from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import {MongoDBAdapter} from "@next-auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+import {Admin} from "@/models/Admin";
+import {mongooseConnect} from '@/lib/mongoose';
+
+const adminEmails = ['malina.neag@gmail.com'];
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    session: async ({session,token,user}) => {
+      if (adminEmails.includes(session?.user?.email)) {
+        return session;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+
+export default NextAuth(authOptions);
+
+export async function isAdminRequest(req,res) {
+  const session = await getServerSession(req,res,authOptions);
+  if (!adminEmails.includes (session?.user?.email)) {
+    throw 'Not an admin';
+  }
+}
